53c94cd1ab42a59f720ee6f4db7ee25eed8158e1
JaeAiKay <78583537+JaeAiKay@users.noreply.github.com>
2022-10-30 18:37:49 +0700

    Update OrderFoodView.vue

--- app/src/views/customer/OrderFoodView.vue
+++ app/src/views/customer/OrderFoodView.vue
@@ -4,9 +4,12 @@ import {useFoodOrderStore} from "@/stores/foodOrder";
 import PlusMinusButtonCard from "@/components/foods/PlusMinusButtonCard.vue";
 import {useOrderStore} from "@/stores/order";
 import {useAuthStore} from "@/stores/auth";
+import Popup from "@/components/foods/Popup.vue";
+import {ref} from "vue";
 
 export default {
   setup() {
+    const popupOpen = ref(false)
     const food_order_store = useFoodOrderStore()
     const order_store = useOrderStore()
     const auth_store = useAuthStore()
@@ -18,11 +21,13 @@ export default {
       error: "",
       auth: null,
       orderId: null,
+      popupOpen: false,
     }
   },
   components: {
     FoodCard,
     PlusMinusButtonCard,
+    Popup,
   },
   async mounted() {
     await this.food_order_store.fetch()
@@ -33,7 +38,8 @@ export default {
   methods: {
     async handleSubmitOrder(){
       await this.newOrder()
-      this.$router.push('/order/'.concat(this.orderId))
+      this.popupOpen = true
+      // this.$router.push('/order/'.concat(this.orderId))
     },
     async newOrder() {
       await this.food_order_store.fetch()
@@ -60,6 +66,12 @@ export default {
     },
     async deleteFoodOrder(foodOrderID) {
       await this.food_order_store.deleteFoodOrder(foodOrderID)
+    },
+    handleBackToHomePage() {
+      this.$router.push(`/foods`)
+    },
+    handleGoToHistoryOrder() {
+      this.$router.push(`/order/`.concat(this.orderId))
     },
   }, watch:{
     food_order_store: {
@@ -97,5 +109,37 @@ export default {
       </div>
     </div>
   </div>
+
+<!--  Popup-->
+  <Popup :open="popupOpen">
+    <template v-slot:header>
+      สั่งอาหารสำเร็จ
+    </template>
+
+    <template v-slot:content>
+      <div class="inline-flex flex-shrink-0 justify-center items-center w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+        <span class="sr-only">Check icon</span>
+      </div>
+      <div class="text-center">
+        <div role="status">
+          <svg class="inline m-6 w-16 h-16 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+          </svg>
+          <span class="sr-only">Loading...</span>
+        </div>
+      </div>
+    </template>
+
+    <template v-slot:footer>
+      <button data-modal-toggle="defaultModal" type="button" @click="handleGoToHistoryOrder" class="text-white bg-blue-700 border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        ประวัติการสั่ง
+      </button>
+      <button data-modal-toggle="defaultModal" type="button" @click="handleBackToHomePage" class="text-white bg-blue-700 border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        หน้าหลัก
+      </button>
+    </template>
+  </Popup>
 </template>
 
