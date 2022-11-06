3de49104759a05771b5f6cbde0d418d8d11b2d49
JaeAiKay <78583537+JaeAiKay@users.noreply.github.com>
2022-11-07 02:03:10 +0700

    Update CreateFoodView.vue

--- app/src/views/foods/CreateFoodView.vue
+++ app/src/views/foods/CreateFoodView.vue
@@ -77,11 +77,11 @@
         </template>
 
         <template v-slot:footer>
-          <button data-modal-toggle="defaultModal" type="button" @click="close" class="bg-gray-200 border border-blue-700 hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            ปิด
-          </button>
           <button data-modal-toggle="defaultModal" type="button" @click="backToHomePage" class="text-white bg-blue-700 border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             หน้าหลัก
+          </button>
+          <button data-modal-toggle="defaultModal" type="button" @click="close" class="bg-gray-200 border border-blue-700 hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            ปิด
           </button>
         </template>
       </Popup>
